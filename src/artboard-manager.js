3df45f62b98ae1f40522bea31ba2873b98f8118f
Ale Muñoz <bomberstudios@gmail.com>
2018-11-19 12:08:57 +0100

    Re-wrote the plugin using the new JS API
    Also took the opportunity to rewrite the whole snapping algorithm so that is works nicely with random-sized artboards

--- src/artboard-manager.js
+++ src/artboard-manager.js
@@ -1,17 +1,19 @@
+const sketch = require('sketch')
+const UI = sketch.UI
 import { getDefaultSettings } from "./artboard-settings.js"
 
 // Config
 let config = getDefaultSettings()
 
 const sort_by_x_position = function(a,b){
-  return a.frame().left() - b.frame().left();
+  return a.frame.x - b.frame.x
 }
 const sort_by_y_position = function(a,b) {
-  return a.frame().top() - b.frame().top();
+  return a.frame.y - b.frame.y
 }
 
 export function ArtboardChanged(context) {
-  console.log("ArtboardChanged")
+  // console.log("ArtboardChanged")
   // Called on
   // - Add
   // - Remove
@@ -28,7 +30,7 @@ export function ArtboardChanged(context) {
 }
 
 const anArtboardIsSelected = function(context){
-  console.log("anArtboardIsSelected")
+  // console.log("anArtboardIsSelected")
   // We need a document, let's try some options:
   const doc = context.document || context.actionContext.document || NSApp.orderedDocuments().firstObject()
   const selectedLayers = doc.selectedLayers().layers()
@@ -44,14 +46,14 @@ const anArtboardIsSelected = function(context){
 }
 
 export function Duplicate(context) {
-  console.log('Duplicate')
+  // console.log('Duplicate')
   if (anArtboardIsSelected(context)) {
     ArrangeArtboards(context)
   }
 }
 
 export function LayersMoved (context) {
-  console.log("LayersMoved")
+  // console.log("LayersMoved")
   const movedLayers = Array.from(context.actionContext.layers)
   let needToArrange = false
 
@@ -66,84 +68,90 @@ export function LayersMoved (context) {
 }
 
 export function ArrangeArtboards(context) {
+  console.log("=======================================================")
   console.log("ArrangeArtboards")
-  const doc = MSDocument.currentDocument()
-  const originalSelection = doc.selectedLayers()
-  const artboards = doc.currentPage().artboards()
+  const doc = sketch.getSelectedDocument()
+  const originalSelection = doc.selectedLayers
+  const artboards = doc.selectedPage.layers.filter(layer => layer.type == 'Artboard')
+  const nativeArtboards = artboards.map(artboard => artboard.sketchObject)
 
-  const layoutBounds = MSLayerGroup.groupBoundsForContainer(MSLayerArray.arrayWithLayers(artboards))
+  const layoutBounds = MSLayerGroup.groupBoundsForContainer(MSLayerArray.arrayWithLayers(nativeArtboards))
+
   const layoutWidth  = layoutBounds.size.width
   const layoutHeight = layoutBounds.size.height
+
+  // This will be the starting point for our Artboard Grid
   const layoutX  = layoutBounds.origin.x
   const layoutY = layoutBounds.origin.y
 
-  const numberOfRows = (layoutHeight / config.gridVerticalSpace).toFixed()
+  // TODO: Dynamic snapping distance, based on Artboard size?
+  var averageHeight = artboards.reduce((initial, artboard) => {
+    initial += artboard.frame.height
+    return initial
+  },0) / artboards.length
+  var maxHeight = artboards.reduce((initial, artboard) => {
+    if (artboard.frame.height > initial) {
+      initial = artboard.frame.height
+    }
+    return initial
+  },0)
 
-  let currentRow = 0
-  let currentRowPosition = layoutY
-  let rows = []
+  var snapDistance = averageHeight
+  var snapDistance = config.snapDistance
+  var snapDistance = maxHeight // I think this one is nicer
 
-  while (currentRow < numberOfRows) {
+  // First, snap all Artboards to the grid, to group them by rows
+  // They may overlap, but we'll fix that in a minute
+  artboards.forEach(artboard => {
+    artboard.frame.y = snapValueToGrid(artboard.frame.y, snapDistance)
+  })
 
-    // console.log("Processing row " + currentRow + ", which starts at position: " + (currentRowPosition - layoutY))
-    let currentRowArtboards = []
-    for (const artboard of Array.from(artboards)) {
-      if(Math.abs(artboard.frame().y() - currentRowPosition) <= config.snapDistance ) {
-        artboard.frame().y = currentRowPosition
-        currentRowArtboards.push(artboard)
+  var artboardRows = artboards.sort(sort_by_y_position).reduce((initial, artboard) => {
+    initial.push(artboard.frame.y)
+    return initial
+  }, [])
+
+  // Set X position for all Artboards on each row
+  const artboardRowValues = [...new Set(artboardRows)]//.sort()
+  var artboardY = layoutY
+  var currentRow = 0
+  artboardRowValues.forEach(rowValue => {
+    var tallestArtboard = 0
+    var artboardX = layoutX
+    var artboardsInRow = artboards.filter(artboard => artboard.frame.y == rowValue)
+    artboardsInRow.sort(sort_by_x_position).forEach((artboard, index) => {
+      var currentColumn = index + 1
+      artboard.frame.x = artboardX
+      artboard.frame.y = artboardY
+      artboardX += artboard.frame.width + config.gridHorizontalSpace
+      if (artboard.frame.height > tallestArtboard) {
+        tallestArtboard = artboard.frame.height
       }
-    }
-    if (currentRowArtboards.length > 0) {
-      rows.push(currentRowArtboards)
-    }
-    currentRowPosition += config.gridVerticalSpace
-    currentRow++
-  }
-
-  // Now, update positions for all artboards
-  let verticalOffset = 0
-  let rowNumber = 0
-  for (const row of rows) {
-
-    // Vertical positions
-    let tallestArtboard = 0
-    for (const artboard of row) {
-      artboard.frame().y = verticalOffset + layoutY
-      tallestArtboard = Math.max(tallestArtboard, artboard.frame().height())
-    }
-    verticalOffset += tallestArtboard + (config.gridVerticalSpace/2)
-
-    // Horizontal positions & name
-    let offsetX = 0
-    let columnNumber = 1
-    for (const artboard of row.sort(sort_by_x_position)) {
-      artboard.frame().x = layoutX + offsetX
-      offsetX += artboard.frame().width() + config.gridHorizontalSpace
       if (config.renameArtboards) {
-        artboard.setName(config.artboardBasenames[rowNumber] + 
-          columnNumber.toLocaleString('en-US', {minimumIntegerDigits: config.minimumIntegerDigits, useGrouping:false}))
+        artboard.name = config.artboardBasenames[currentRow] + currentColumn.toLocaleString('en-US', {minimumIntegerDigits: config.minimumIntegerDigits, useGrouping:false})
       }
-      columnNumber++
-    }
-    rowNumber++
-  }
+    })
+    artboardY += tallestArtboard + config.gridVerticalSpace
+    currentRow++
+  })
 
   // Update stacking
-  for (const artboard of Array.from(artboards).sort(sort_by_x_position).sort(sort_by_y_position)) {
-    const parent = artboard.parentGroup()
-    artboard.removeFromParent()
-    parent.insertLayer_atIndex(artboard, 0)
-  }
+  artboards.sort(sort_by_x_position).sort(sort_by_y_position).forEach(artboard => {
+    const parent = artboard.parent
+    artboard.remove()
+    parent._object.insertLayer_atIndex(artboard._object, 0)
+  })
+
   // Restore original selection
-  for (const layer of Array.from(originalSelection)) {
-    layer.isSelected = true
-  }
+  originalSelection.forEach(artboard => artboard.selected = true)
+
+  UI.message('Artboards arranged')
 }
 
 export function Resize(context){
-  console.log("Resize")
+  // console.log("Resize")
   // console.log(context)
-  console.log(context.actionContext)
+  // console.log(context.actionContext)
   // "Normal" — User selected the Artboard tool to add an Artboard 🤔
   if (context.actionContext.name == "NormalResize" || context.actionContext.name == "NormalMultipleResize" || context.action == "ResizeArtboardToFit.finish") {
     if (anArtboardIsSelected(context)) {
@@ -162,4 +170,14 @@ export function ResizeArtboardToFit(context){
 
 export function InsertArtboard(context){
   console.log('InsertArtboard');
+}
+
+function snapValueToGrid(value, grid) {
+  var div = value/grid
+  // Also snap down, so the Artboard doesn't jump to the row above if we're only 1 px above the snapping line
+  var rest = div - Math.floor(div)
+  if (rest > 0.8) {
+    div += 1
+  }
+  return Math.floor(Math.floor(div) * grid)
 }
