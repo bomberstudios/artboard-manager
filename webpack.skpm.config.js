6db7d3695abd641829309fd7805a52b075aacc66
Ale M <bomberstudios@gmail.com>
2019-12-10 10:57:51 +0100

    Fix for GHSA-h9rv-jmmf-4pgx

--- webpack.skpm.config.js
+++ webpack.skpm.config.js
@@ -1,29 +1,32 @@
-module.exports = function(config) {
-  config.resolve.extensions = [".sketch.js", ".js"]
+module.exports = function (config, entry) {
+  config.node = entry.isPluginCommand ? false : {
+    setImmediate: false
+  };
   config.module.rules.push({
     test: /\.(html)$/,
-    use: [
-      {
-        loader: "@skpm/extract-loader"
+    use: [{
+        loader: "@skpm/extract-loader",
       },
       {
         loader: "html-loader",
         options: {
-          attrs: ["img:src", "link:href"],
-          interpolate: true
-        }
-      }
+          attrs: [
+            'img:src',
+            'link:href'
+          ],
+          interpolate: true,
+        },
+      },
     ]
   })
   config.module.rules.push({
     test: /\.(css)$/,
-    use: [
-      {
-        loader: "@skpm/extract-loader"
+    use: [{
+        loader: "@skpm/extract-loader",
       },
       {
-        loader: "css-loader"
-      }
+        loader: "css-loader",
+      },
     ]
   })
 }
